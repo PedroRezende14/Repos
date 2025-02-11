4f965c5873009b7003053c4b7b49c91ca00554b9
PedroRezende14 <pedro.rezende14@outlook.com>
2025-02-11 19:46:10 -0300

    Adicionando função de loading, mudando a ft da capa e a sessao de hab

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Seleciona todos os links de navegação
+
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
-        e.preventDefault(); // Evita o comportamento padrão do link
+        e.preventDefault(); 
 
-        // Faz a rolagem suave até a seção correspondente
+  
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
